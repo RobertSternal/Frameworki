da895a716a515297fcbae0b3d2f1fce6239ee326
RobertSternal <robst@onet.pl>
2022-10-25 17:58:27 +0200

    25.10 v3

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="posts" element={<Posts />}></Route>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+    </>
   );
 }
 
-export default App;
